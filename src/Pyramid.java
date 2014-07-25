fb0c90621959f9305182fd8ea5368df73d673e8c
Ricky Harkins <r.harkins@bullhorn.com>
2014-07-25 09:07:04 -0500

    Moving primary Pyramid construction code to PyramidBuilder

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -8,30 +8,11 @@ public class Pyramid {
     private int size;
 
     public Pyramid() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = 0;
-        do {
-            try {
-                System.out.println("How tall do you want your pyramind?");
-                size = Integer.parseInt(br.readLine());
-            } catch (Exception e) {
-                System.out.println("Not a valid number");
-            }
-        } while (!isValid(size));
 
     }
 
     public Pyramid(int size) {
         this.size = size;
-    }
-
-    private static boolean isValid(int n) {
-        if (n < 1 || n > 23) {
-            System.out.println("Not a valid number");
-            return false;
-        } else {
-            return true;
-        }
     }
 
     public String toString() {
--- src/PyramidBuilder.java
+++ src/PyramidBuilder.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PyramidBuilder {
 
     private PyramidBuilder() {
@@ -13,6 +16,26 @@ public class PyramidBuilder {
     }
 
     public Pyramid pyramid() {
-        return new Pyramid();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        int size = 0;
+        do {
+            try {
+                System.out.println("How tall do you want your pyramind?");
+                size = Integer.parseInt(br.readLine());
+            } catch (Exception e) {
+                System.out.println("Not a valid number");
+            }
+        } while (!isValid(size));
+        return new Pyramid(size);
+    }
+
+    private static boolean isValid(int n) {
+        if (n < 1 || n > 23) {
+            System.out.println("Not a valid number");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
